3e257cc07d05ac31280177aeeb930c5fe17b3d8d
choiuhana <choiuhana@gmail.com>
2022-07-12 19:27:03 +0900

    config 일부 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
     siteMetadata: {
         title: `주니어 개발자의 개발 블로그`,
         description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
-        author: `Hyun`,
-        siteUrl: 'https://my-website-link.com', // 배포 후 변경 예정
+        author: `choiuhana`,
+        siteUrl: 'https://choiuhana.github.io/',
     },
     plugins: [
         {
@@ -85,7 +85,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-canonical-urls',
             options: {
-                siteUrl: '<https://my-website.com/>',
+                siteUrl: 'https://choiuhana.github.io/',
                 stripQueryString: true,
             },
         },
