2cca7d40deca35d4d7536453c7b8977520f7957f
choiuhana <choiuhana@gmail.com>
2022-07-24 19:58:01 +0900

    글꼴 추가 및 변경
    - npm install @fontsource/gothic-a1
    - 기본폰트 변경(나눔명조 -> 고딕 a1)

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react';
-import { Global, css } from '@emotion/react';
+import React, {FunctionComponent} from 'react';
+import {Global, css} from '@emotion/react';
+import '@fontsource/gothic-a1'; // Defaults to weight 400 with all styles included.
 
 const defaultStyle = css`
     @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap');
@@ -8,7 +9,7 @@ const defaultStyle = css`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: 'Nanum Myeongjo', serif;
+        font-family: 'Gothic A1', serif;
     }
 
     html,
